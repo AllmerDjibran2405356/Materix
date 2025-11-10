28dc76fbb6208d811b9286b367225081fad74b98
Deviaulia0609 <devvaulrach@gmail.com>
2025-11-10 14:24:37 +0700

    Update routes to return correct views for landing, masuk, and daftar pages

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('homepage');
+    return view('Page.landing');
 });
+
+Route::get('/masuk', function () {
+    return view('Page.masuk');
+})->name('masuk');
+
+Route::get('/daftar', function () {
+    return view('Page.daftar');
+})->name('daftar');
