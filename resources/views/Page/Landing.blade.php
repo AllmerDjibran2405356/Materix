83ca428d404015e01bdaeb273bd5c80a67993d09
RotatoryEarth <bumib22@gmail.com>
2025-11-14 23:16:07 +0700

    update: vie/page landing landing.css public/image

--- resources/views/Page/Landing.blade.php
+++ resources/views/Page/Landing.blade.php
@@ -8,7 +8,7 @@
 {{-- ================= Container 1 ================= --}}
 <section class="hero-container">
     <div class="hero-overlay"></div>
-    <img src="{{ asset('images/landingpage.png') }}" alt="Materix Building" class="hero-bg">
+    <img src="{{ asset('images/landingpage.jpg') }}" alt="Materix Building" class="hero-bg">
     <div class="hero-text">
         <h1>Optimalkan Setiap Material,<br>Maksimalkan Hasil Pembangunan.</h1>
     </div>
@@ -16,15 +16,19 @@
 
 {{-- ================= Container 2 ================= --}}
 <section class="about-container">
+    <div class="about-title">
+        <h2>Tentang MateRix</h2>
+    </div>
+
     <div class="about-content">
         <div class="about-text">
-            <h2>Tentang MateRix</h2>
             <p>
-                Materix adalah sistem berbasis web untuk menghitung kebutuhan material konstruksi secara otomatis.
-                Aplikasi ini menganalisis desain bangunan dan mengintegrasikan data harga bahan untuk menghasilkan estimasi biaya proyek yang akurat.
-                Dengan Materix, perencanaan konstruksi jadi lebih cepat, efisien, dan minim kesalahan.
+                MateRix adalah sistem berbasi web untuk menghitung kebutuhan material konstruksi secara otomatis.
+                Aplikasi ini menganalisis desain bangunan dan mengintegrasikan data harga bahan untuk menghasilkan estimasi biaya proyek yang akurat. 
+                Dengan MateRix, perencanaan konstruksi jadi lebih cepat, efisien, dan minim kesalahan.
             </p>
         </div>
+
         <div class="about-image">
             <img src="{{ asset('images/building.jpg') }}" alt="Gedung Materix">
         </div>
@@ -47,7 +51,7 @@
             </div>
             <div class="feture-text">
                 <h3>Digitalisasi & Analisis Desain Bangunan</h3>
-                <p>Anda dapat dengan mudah menginputkan desain bangunan untuk mendapatkan estimasi otomatis mengenai kebutuhan bahan yang diperlukan dalam proyek.</p>
+                <p>Anda dapat dengan mudah mengunggah desain bangunan, untuk mendapatkan analisis otomatis mengenal kebutuhan material yang diperlukan dalam proyek.</p>
             </div>
         </div>
 
@@ -57,7 +61,7 @@
             </div>
             <div class="feture-text">
                 <h3>Kelola Data Bahan dan Supplier</h3>
-                <p>Materix membantu Anda mengontrol harga bahan dan memilih supplier yang sesuai dengan estimasi proyek agar efisien dan tepat anggaran.</p>
+                <p>Melalui fitur ini, anda bisa memperbarui harga bahan dan memilih supplier yang sesuai, sehingga perhitungan anggaran proyek menjadi lebih akuran dan terkini.</p>
             </div>
         </div>
 
@@ -67,7 +71,7 @@
             </div>
             <div class="feture-text">
                 <h3>Perhitungan & Laporan Estimasi Proyek</h3>
-                <p>Pengguna dapat melihat hasil estimasi dan laporan langsung yang dapat diunduh sebagai PDF untuk kebutuhan dokumentasi dan presentasi proyek.</p>
+                <p>Pengguna dapat melihat hasil dalam bentuk laporan lengkap yang bisa diunduh sebagai PDF atau Excel untuk dokumentasi dan perencanaan.</p>
             </div>
         </div>
 
