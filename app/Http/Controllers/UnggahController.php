d7bfaf9707e51f131927cdea9730534be3a43655
Deviaulia0609 <devvaulrach@gmail.com>
2025-11-15 20:42:32 +0700

    kebutuhan unggah

--- app/Http/Controllers/UnggahController.php
+++ app/Http/Controllers/UnggahController.php
@@ -5,7 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 Class UnggahController extends Controller{
+    public function index(){
+        return view('unggah.index');
+    }
 
-    
+    public function upload(Request $request){
+        $request->validate([
+            'file' => 'required|extensions:ifc',
+        ]);
 
+        $file = $request->file('file');
+        $namaFile = time().'_'.$file->getClientOriginalName();
+        $file ->move(public_path('uploads/file'), $namaFile);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'File berhasil diunggah!',
+            'filename' => $namaFile
+        ]);
+    }
 }
---
+++ resources/views/Page/Unggah.blade.php
@@ -0,0 +1,13 @@
+<div id="drop-zone" 
+class="p-5 rounded-4 shadow"
+style="background:#062B77; border:3px dashed #fff; cursor:pointer;">
+<h4 class="text-white fw-bold mb-3">
+    Tarik dan letakkan gambar di sini
+</h4>
+
+<button class="btn btn-warning mt-3" id="triggerInput">
+    Unggah berkas
+</button>
+
+<input type="file" id="fileInput" hidden>
+</div>
