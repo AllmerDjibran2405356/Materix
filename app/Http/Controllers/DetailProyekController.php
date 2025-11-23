d75f816b15c3936ff7fe343b42a9abdd4abc1db4
Deviaulia0609 <devvaulrach@gmail.com>
2025-11-23 22:58:21 +0700

    bismillah bisa bagian detail proyek

--- app/Http/Controllers/DetailProyekController.php
+++ app/Http/Controllers/DetailProyekController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use App\Models\DesainRumah;
 
 class DetailProyekController extends Controller
@@ -9,10 +10,10 @@ class DetailProyekController extends Controller
    public function show(int $id)
 {
     $project = DesainRumah::where('ID_Desain_Rumah', $id)
-        ->where('id_user', auth()->id())
+        ->where('id_user', Auth::id())
         ->firstOrFail();
 
-    return view('page.proyek_detail', compact('Project'));
+    return view('page.proyek_detail', compact('project'));
 }
 
 }
