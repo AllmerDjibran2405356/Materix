6979b14447e0ea77fe3ea51bbabc64c8fc509c3b
AllmerDjibran2405356 <allmer.djibran.2405356@students.um.ac.id>
2025-12-03 19:44:45 +0700

    menambahkan tabel data  ke pop-up supplier

--- app/Http/Controllers/KalkulasiController.php
+++ app/Http/Controllers/KalkulasiController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\DesainRumah;
 use App\Models\RekapKebutuhanBahanProyek;
+use App\Models\ListHargaBahan;
+use App\Models\ListSupplier;
+use App\Models\SupplierAlamat;
+use App\Models\SupplierKontak;
 use Illuminate\Http\Request;
 
 class KalkulasiController extends Controller
@@ -14,9 +18,12 @@ class KalkulasiController extends Controller
         $recaps = RekapKebutuhanBahanProyek::where('ID_Desain_Rumah', $id)
         ->get();
 
+        $suppliers = ListSupplier::orderBy('Nama_Supplier')
+        ->get();
+
         $message = $recaps->isEmpty() ? "empty data" : null;
 
-        return view('Page.DataBahanDanProdusen', compact('recaps', 'message'));
+        return view('Page.DataBahanDanProdusen', compact('recaps', 'suppliers', 'message'));
     }
 }
 
--- resources/views/Page/DataBahanDanProdusen.blade.php
+++ resources/views/Page/DataBahanDanProdusen.blade.php
@@ -124,14 +124,32 @@
   <div class="modal-dialog">
     <div class="modal-content">
 
-      <div class="modal-header">
-        <h5 class="modal-title">Data Supplier</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-      </div>
+        <div class="modal-header">
+            <h5 class="modal-title">Data Supplier</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+        </div>
 
-      <div class="modal-body">
-        <p>Template data supplier di sini...</p>
-      </div>
+        <div class="modal-body">
+            <a>Tambah Supplier</a>
+
+            <table class="table table-bordered table-sm">
+                <thead>
+                    <tr>
+                        <th>ID Supplier</th>
+                        <th>Nama Supplier</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($suppliers as $supplier)
+                        <tr>
+                            <td>{{ $supplier->ID_Supplier }}</td>
+                            <td>{{ $supplier->Nama_Supplier }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
 
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
